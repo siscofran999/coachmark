67a1f7b090c646a85a08657773245c9f6e7418cf
siscofran999 <siscofran999@gmail.com>
2023-11-10 13:48:41 +0700

    update README.md + try implement

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation(project(mapOf("path" to ":coachmark")))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -68,4 +67,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("com.github.siscofran999:coachmark:0.9-beta")
 }
